ef943a26523e5c51069e2999e9d8e4bd89dc07e8
Mariacris155 <mariacris15595@gmail.com>
2022-07-14 18:43:42 +0200

    ejercicio4

---
+++ 3-state/ej4/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import {useState} from 'react'
+
+function App() {
+  let [contador, setContador] = useState (0)
+  return (
+    <><h1>{contador}</h1>
+    <button onClick = {() => setContador(++contador)>}>+</button>
+    <button onClick = {() => setContador(--contador)>}>-</button>
+    </>
+   
+  );
+}
+
+export default App;
